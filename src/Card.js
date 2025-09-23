c880fed980f1579ba4ca96f724595a63fe29b0f8
michaeltercias <michaeltercias@gmail.com>
2025-09-23 12:47:51 +0800

    fixed indentation

--- src/Card.js
+++ src/Card.js
@@ -3,27 +3,27 @@ import styles from "./Card.module.scss";
 import classNames from "classnames";
 
 export default function Card({ card, handleChoice, flipped, disabled }) {
-  const handleClick = () => {
-    if (!disabled) {
-      handleChoice(card);
-    }
-  };
+    const handleClick = () => {
+        if (!disabled) {
+            handleChoice(card);
+        }
+    };
 
-  return (
-    <button
-      className={classNames(styles.card, { [styles.flipped]: flipped })}
-      onClick={handleClick}
-      disabled={disabled || card.matched}
-      aria-pressed={flipped}
-    >
-      <div className={styles.cardInner}>
-        <div className={classNames(styles.cardFace, styles.front)}>
-          {card.emoji}
-        </div>
-        <div className={classNames(styles.cardFace, styles.back)}>
-          <span className={styles.backMark}>?</span>
-        </div>
-      </div>
-    </button>
-  );
+    return (
+        <button
+            className={classNames(styles.card, { [styles.flipped]: flipped })}
+            onClick={handleClick}
+            disabled={disabled || card.matched}
+            aria-pressed={flipped}
+        >
+            <div className={styles.cardInner}>
+                <div className={classNames(styles.cardFace, styles.front)}>
+                    {card.emoji}
+                </div>
+                <div className={classNames(styles.cardFace, styles.back)}>
+                    <span className={styles.backMark}>?</span>
+                </div>
+            </div>
+        </button>
+    );
 }
